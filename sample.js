34aeff7a73b242def9eb70968ca45feb9acca846
sanasham <sayyedesubvali@gmail.com>
2023-07-13 17:34:36 +0530

    practice code

---
+++ sample.js
@@ -0,0 +1,81 @@
+// let name1 = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+// };
+// let printName = function (homeTown) {
+//   console.log(
+//     'full name: ',
+//     this.firstName + ' ' + this.lastName,
+//     'HomeTown ' + homeTown
+//   );
+// };
+// //var firstName = 'Rajan';
+// //var lastName = 'Rao';
+// printName.call(name1, 'vijaywada', 'parisi');
+// //printName();
+
+// let name2 = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+// };
+// function abc() {
+//   console.log(age);
+//   let age;
+//   age = 30;
+// }
+// abc();
+
+// const employee = {
+//   id: 123,
+//   name: 'Doe',
+//   dept: 'engineering',
+// };
+
+// const { id, name, Dept } = employee;
+// console.log(id, name, dept);
+
+// const person = {
+//   name1: 'Vali',
+//   age: 40,
+//   address: {
+//     city: { taluk: 'vij', post: 'ventra', mandal: 'pedaparupudi' },
+//     state: 'ap',
+//     country: 'india',
+//   },
+// };
+// const {
+//   name1,
+//   age,
+//   address: {
+//     city: { taluk, post, mandal },
+//     state,
+//     country,
+//   },
+// } = person;
+// console.log(name1, age, taluk, post, mandal, state, country);
+
+// console.log(age);
+// var age = 20;
+// console.log(age);
+// let age = 20;
+
+// text = '1' + 'happy';
+// console.log(text);
+// console.log(typeof text);
+// const a;
+// a=40;
+// function Person() {
+//   console.log('sample');
+// }
+// Person.prototype.name = 'Sudheer';
+// var object1 = new Person();
+// console.log(object1);
+
+let a = 100;
+
+if (a > 500) {
+  var b = 100;
+} else {
+  var b = 700;
+}
+console.log(b);
